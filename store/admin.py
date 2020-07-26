457b54a977b2892a53c89b7f6e0862b373cd6f86
Teck Mee_May <kongteckmee@gmail.com>
2020-07-26 10:36:13 +0000

    Customized admin

--- store/admin.py
+++ store/admin.py
@@ -2,4 +2,19 @@ from django.contrib import admin
 from .models import Course
 
 # Register your models here.
-admin.site.register(Course)
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku' ,
+        'name' ,
+        'description_1' ,
+        'description_2' ,
+        'description_3' ,
+        'description_4' ,
+        'price' ,
+        'image' ,
+    )
+
+    ordering = ('sku',)
+
+admin.site.register(Course, CourseAdmin)
