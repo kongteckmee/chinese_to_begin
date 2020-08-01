61a8b00d2b05f2a4e8d50e135dbf66c2e5c6e9a6
Teck Mee_May <kongteckmee@gmail.com>
2020-08-01 15:09:21 +0000

    Added category model and customized admin

--- store/admin.py
+++ store/admin.py
@@ -1,20 +1,27 @@
 from django.contrib import admin
-from .models import Store
+from .models import Store, Category
 
 # Register your models here.
 
 class StoreAdmin(admin.ModelAdmin):
     list_display = (
-        'sku' ,
-        'name' ,
-        'description_1' ,
-        'description_2' ,
-        'description_3' ,
-        'description_4' ,
-        'price' ,
-        'image' ,
+        'sku',
+        'name',
+        'description_1',
+        'description_2',
+        'description_3',
+        'description_4',
+        'before_discount',
+        'price',
+        'image',
     )
 
     ordering = ('sku',)
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+    )
+
 admin.site.register(Store, StoreAdmin)
+admin.site.register(Category, CategoryAdmin)
--- store/models.py
+++ store/models.py
@@ -2,13 +2,25 @@ from django.db import models
 
 # Create your models here.
 
+class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
 class Store(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=20, null=True, blank=True)
     name = models.CharField(max_length=254)
     description_1 = models.TextField()
     description_2 = models.TextField()
     description_3 = models.TextField()
     description_4 = models.TextField()
+    before_discount = models.DecimalField(max_digits=6, decimal_places=0)
     price = models.DecimalField(max_digits=6, decimal_places=0)
     image = models.ImageField(null=True, blank=True)
 
