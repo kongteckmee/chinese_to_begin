819e24a705daa4243e0260bb93b82abf68e276a1
Teck Mee_May <kongteckmee@gmail.com>
2020-08-07 13:04:13 +0000

    Added course detail functionality

--- store/urls.py
+++ store/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_courses, name='store')
+    path('', views.all_courses, name='store'),
+    path('<store_id>', views.course_detail, name='course_detail'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Store
 
 # Create your views here.
@@ -13,3 +13,14 @@ def all_courses(request):
     }
     
     return render(request, 'store/store.html', context)
+
+def course_detail(request, store_id):
+    """ A view to show individual course details """
+
+    store = get_object_or_404(Store, pk=store_id)
+
+    context = {
+        'store': store,
+    }
+    
+    return render(request, 'store/course_detail.html', context)
