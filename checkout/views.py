049f84b4aa29900990e395edd625a1e7e950fc10
Teck Mee_May <kongteckmee@gmail.com>
2020-08-08 01:15:13 +0000

    Modified course detail page

--- checkout/views.py
+++ checkout/views.py
@@ -3,7 +3,7 @@ from .forms import OrderForm
 
 # Create your views here.
 
-def checkout(request):
+def checkout(request, store_id):
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
