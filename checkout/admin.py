0d29de23f2d5ce795b896bc74006559023c2e0ee
Teck Mee_May <kongteckmee@gmail.com>
2020-08-09 05:39:19 +0000

    Added checkout css & rearranged payment form

--- checkout/admin.py
+++ checkout/admin.py
@@ -13,9 +13,9 @@ class OrderAdmin(admin.ModelAdmin):
 
     readonly_fields = ('order_number', 'date', 'order_total',)
     fields = ('order_number', 'date', 'full_name', 
-            'email', 'phone_number', 'street_address_1', 'street_address_2', 
-            'town_or_city', 'county', 'postcode',
-            'country', 'order_total',)
+            'email', 'phone_number', 'country', 'street_address_1', 
+            'street_address_2', 'postcode',
+            'town_or_city', 'county', 'order_total',)
     
     list_display = ('order_number', 'date', 'full_name',
                     'email', 'phone_number', 'order_total',)
