c76b41a04551e1623cb9804640153ab08178ab49
Teck Mee_May <kongteckmee@gmail.com>
2020-08-14 14:23:06 +0000

    Updated allauth templates and profile app

--- profiles/models.py
+++ profiles/models.py
@@ -31,4 +31,4 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
     # Existing user: Save the profile
-    instance.UserProfile.save()
+    instance.userprofile.save()
