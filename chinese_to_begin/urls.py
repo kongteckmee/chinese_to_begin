fcc3625e672fb985d9a1a4ae9bfb40e8184c3b9e
Teck Mee_May <kongteckmee@gmail.com>
2020-08-14 10:56:22 +0000

    Added profiles app

--- chinese_to_begin/urls.py
+++ chinese_to_begin/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('store/', include('store.urls')),
     path('about/', include('about.urls')),
     path('checkout/', include('checkout.urls')),
+    path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
