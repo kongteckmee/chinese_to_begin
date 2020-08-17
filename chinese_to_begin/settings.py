fabd8044dc60cbc5f77166ea8f876e47925165f0
Teck Mee_May <kongteckmee@gmail.com>
2020-08-17 15:54:34 +0000

    Deleted line in setting

--- chinese_to_begin/settings.py
+++ chinese_to_begin/settings.py
@@ -191,7 +191,6 @@ if 'USE_AWS' in os.environ:
 
     # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
-    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
     STATICFILES_LOCATION = 'static'
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
