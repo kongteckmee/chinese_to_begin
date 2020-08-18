428fb6f3f7c3bb23ec33aa97a78c3cd917901534
Teck Mee_May <kongteckmee@gmail.com>
2020-08-18 13:19:13 +0000

    Changed position of email settings

--- chinese_to_begin/settings.py
+++ chinese_to_begin/settings.py
@@ -210,9 +210,9 @@ if 'DEVELOPMENT' in os.environ:
     DEFAULT_FROM_EMAIL = 'chinesetobegin@example.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_USE_TLS = True
-    EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_PORT = 465
+    EMAIL_USE_TLS = True
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASS = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
