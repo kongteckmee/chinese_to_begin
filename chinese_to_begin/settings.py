afd30848e9d8cbc045c5e47d0e3f0e54bc0c5e1d
Teck Mee_May <kongteckmee@gmail.com>
2020-08-27 00:26:40 +0000

    Debug to true

--- chinese_to_begin/settings.py
+++ chinese_to_begin/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 
 ALLOWED_HOSTS = ['chinese-to-begin.herokuapp.com', 'localhost']
